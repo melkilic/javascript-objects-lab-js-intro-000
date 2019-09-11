248adf6ec952d17c6fc36b0e97f83c5278668e6d
Melike KILIC <melike180400@gmail.com>
2019-09-11 20:55:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes= new Object();
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({},object,{[key]: value})
 }
-function destructivelyupdateObjectWithKeyAndValue(recipes'prop', '2') {
-  recipes[prop] = '2'
- return recipes
+function destructivelyupdateObjectWithKeyAndValue(object, key, value){
+ object[key] = value
+return object
 }
