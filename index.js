e5cb7715cd88004aa0545aa78ab768efdac8102f
Melike KILIC <melike180400@gmail.com>
2019-09-11 20:34:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes= {flavour:'5'};
 var recipes= new Object({eggs:'3'});
 function updateObjectWithKeyAndValue(recipes,eggs,3){
-  Object.assign({flavour:5},{eggs:'3'})
+  Object.assign({flavour:5},{eggs:3})
 }
