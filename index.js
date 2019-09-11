66d8830b4219d9d033c91a1a344096584e5e451a
Melike KILIC <melike180400@gmail.com>
2019-09-11 20:57:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-var recipes= new Object();
+// var recipes= new Object();
+// function updateObjectWithKeyAndValue(object, key, value){
+//   return Object.assign({},object,{[key]: value})
+// }
+// function destructivelyupdateObjectWithKeyAndValue(object, key, value){
+//  object[key] = value
+// return object
+// }
+var recipes = new Object();
+
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({},object,{[key]: value})
 }
-function destructivelyupdateObjectWithKeyAndValue(object, key, value){
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
  object[key] = value
 return object
 }
